d5d5904505082e03bd584f66d8268e74cccdf019
Volodymyr <vgrinda97@gmail.com>
2014-07-14 10:44:10 +0300

    Database for Newspaper Blog
    Database

---
+++ Blog1/Blog1/Models/PersonalData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Blog1.Models
+{
+    public class PersonalData
+    {
+        [Key]
+        public int PersonalDataID { get; set; }
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+        public DateTime PersDataDate { get; set; }
+        public string PersDataAdress { get; set; }
+        public string PersDataOther { get; set; }
+        public virtual User User { get; set; }
+    }
+}
