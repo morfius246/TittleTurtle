ebb701414a705bf8bf76afa01a109e2044678b9a
romanBionic <rmnleschenko@mai.ru>
2014-07-16 13:48:54 +0300

    Revert "Libs UpToDate"
    This reverts commit 647acde364e66b197f97acb8c7859292630bac1d.

--- Blog1/DBTest/UnitTest1.cs
+++ Blog1/DBTest/UnitTest1.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Data.SqlClient;
 using System.Data.EntityClient;
 using System.Data.Metadata.Edm;
+
 using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -18,7 +19,56 @@ namespace DBTest
         [TestMethod]
         public void TestMethod1()
         {
+            // Specify the provider name, server and database.
+            string providerName = "System.Data.SqlClient";
+            string serverName = @"(LocalDb)\v11.0";
+            string databaseName = "HomeDB";
 
+            // Initialize the connection string builder for the
+            // underlying provider.
+            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
+
+            // Set the properties for the data source.
+            sqlBuilder.DataSource = serverName;
+            sqlBuilder.InitialCatalog = databaseName;
+            sqlBuilder.IntegratedSecurity = true;
+
+            // Build the SqlConnection connection string.
+            string providerString = sqlBuilder.ToString();
+
+            // Initialize the EntityConnectionStringBuilder.
+            var entityBuilder = new EntityConnectionStringBuilder();
+
+            //Set the provider name.
+            entityBuilder.Provider = providerName;
+
+            // Set the provider-specific connection string.
+            entityBuilder.ProviderConnectionString = providerString;
+
+            // Set the Metadata location.
+            entityBuilder.Metadata = @"res://*/HomeDB.csdl|
+                                       res://*/HomeDB.ssdl|
+                                       res://*/HomeDB.msl";
+            MessageBox.Show(entityBuilder.ToString());
+
+
+            using (var ec = new EntityConnection())
+            {
+                try
+                {
+                    ec.ConnectionString = entityBuilder.ToString();
+                    ec.Open();
+                    var ecm = new EntityCommand("INSERT INTO User (UserFirstName, UserLastName) VALUES ('Vsya', 'Pupkin')", ec);
+                    ecm.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+                
+                
+            }
         }
     }
 }
