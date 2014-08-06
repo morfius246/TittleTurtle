85c6a7c9d0ccbcb4b956d7f934b1b0094ab4b7fa
romanBionic <rmnleschenko@mai.ru>
2014-08-06 17:07:35 +0300

    adde ImageReturner

---
+++ TitleTurtle/TitleTurtle/Controllers/ImageReturner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TitleTurtle.Controllers
+{
+    public class ImageReturner : ActionResult
+    {
+        byte[] img;
+
+        public ImageReturner(byte[] media)
+        {
+            img = media;
+        }
+
+        public ImageReturner(string str)
+        {
+            using (var ms = new MemoryStream())
+            {
+                Image tmp = Image.FromFile(str);
+                tmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                img = ms.ToArray();
+            }
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType = "text/plain;";               
+            response.OutputStream.Write(img, 0, img.Count());
+        }
+    }
+}
